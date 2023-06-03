7a493d9b2158ea4eff201eeff678e39c8747e25e
Vahe_from_42VM <vahe_42_VM@mail.ru>
2023-06-03 11:32:42 -0400

    last version

--- project/srcs/requirements/wordpress/conf/create_user.sh
+++ project/srcs/requirements/wordpress/conf/create_user.sh
@@ -1,3 +1,14 @@
-wp core install --url=YOUR_SITE_URL --title="my insception" --admin_user=admin --admin_password=${DB_ROOT} --admin_email=admin@example.com
-wp user create johnik4 john4@example.com --role=subscriber --user_pass=pass
+wp core install --url=localhost --title="my insception" --admin_user=vagevorg --admin_password=${DB_ROOT} --admin_email=admin@example.com
+if wp user get Johnik >/dev/null 2>&1; then
+    echo "User Johnik exists."
+else
+   wp user create Johnik john@example.com --role=subscriber --user_pass=pass
+fi
+wp user update "vagevorg" --user_pass="${DB_ROOT}" --skip-email
+if wp user get "vagevorg" >/dev/null 2>&1; then
+    echo "Logged in successfully as root."
+else
+    echo "Login failed. Please check your root credentials."
+fi
+wp theme activate twentytwentytwo
 /usr/sbin/php-fpm8 -F
